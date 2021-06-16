fad0edb521b23f4136108352193c0143d407b094
noufal1169 <83015862+noufal1169@users.noreply.github.com>
2021-06-16 15:22:40 +0530

    Add files via upload

---
+++ 1 - Hello World Program.py
@@ -0,0 +1,38 @@
+#                           -------------------
+#                           HELLO WORLD PROGRAM
+#                           -------------------
+
+# print() - to print something
+
+print("hello world")
+
+x = "hello"
+y="world"
+
+print(x)
+
+print(x,y)
+
+print(x,y,sep="__") #sep in default is white space
+
+print("\n")
+
+# \n means new line
+# \t means tab
+
+print("hello, \ntoday we are learning about python. \n\tPython is very easy to learn")
+
+print("\n")
+
+print("hello",end=" ") #end in default is \n
+
+print("world")
+
+
+# input() - to take input from the user
+
+a = input()
+print(a)
+
+b = input('enter the value :')
+print(b)
